3aab4634409b5801bb9910f6b53bd8bcd723bf14
StrugVN <37741395+StrugVN@users.noreply.github.com>
2021-04-06 04:28:40 +0700

    1 số kq test

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ trans_funcs = [None, PCA(n_components=3, random_state=0), SAX()]
 # Phải có ít nhất 4 ft để dùng PCA
 
 base_param = {
-    'stock_list': ["GOOGL"],
+    'stock_list': ['JPM', "GOOGL", "DIS", "JNJ", "MMM", "KO", "GE"],
     'target_col': 'Close_proc',
     'similarity_col': 'Close_proc',
     'sim_func': 'co-integration',
@@ -54,7 +54,7 @@ base_param = {
     'k': 50,
     'next_t': 1,
     'selected_features': ['Close_proc'],
-    'window_len': 10,
+    'window_len': 0,
     'model_name': 'GradientBoostingRegressor',
     'n_fold': 5,
     'eval_result_path': '_test.csv',
