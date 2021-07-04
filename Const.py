fb859e05caad954cdaaf68c40ef971eff2ad7510
StrugVN <37741395+StrugVN@users.noreply.github.com>
2021-07-04 21:27:14 +0700

    final x2

--- Const.py
+++ Const.py
@@ -1,9 +1,4 @@
 data_name = 'all_stocks_5yr'
-'''
-GBR semi underfit | meh
-XBGR underfit
-LSTM 16
-'''
 
 #data_name = 'all_stocks_last_1yr'
 
@@ -11,11 +6,3 @@ const_time_col = 'Date'
 const_target_col = 'Close'
 const_name_col = 'Name'
 const_LSTM_saved_weight = 'LSTM_cp.hdf5'
-
-'''
-LSTM batch 32
-'''
-
-"""
- - try XGBC 300, 0.02 - all
-"""
